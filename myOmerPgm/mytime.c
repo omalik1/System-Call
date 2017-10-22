4a2bc483ab8b1ee5438bd1cc355141a71f63268d
csvb <csvb@CS550-omalik1.csvb.local>
2017-10-22 17:27:46 -0400

    adding part C

---
+++ myOmerPgm/mytime.c
@@ -0,0 +1,74 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+#define SUCCESS 0
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("OMER MALIK");
+
+static int device_mj_num = 0;
+static const char dev_name[] = "Get-Time";
+static const char myT[400];
+static const ssize_t time_string_size = sizeof(myT); 
+
+static struct file_operations my_f
+{
+	.read = my_dev_read
+};
+
+/* To load a linux driver */
+static int my_init(void)
+{
+	register_device();	
+	return 0;
+}
+
+static ssize_t my_dev_read( struct file *file_ptr,
+			    char __user *user_buffer,
+			    size_t count,
+			    loff_t *position)
+{
+	struct timespec time1 = current_kernel_time();
+	struct timespec time2;
+	getnstimeofday(&time);
+	sprintf(myT, "current_kernel_time: %ld\n getnstimeofday: %ld\n",time1.tv_nsec, time2.tv_nsec);
+	printl(KERN_INFO, "%s", myT);
+	copy_to_user(user_buffer,myT,strlen(myT)+1);
+	return strlen(myT);
+
+}
+
+
+/* Registering a device */
+static int register_device(void)
+{
+	int register_num = 0;
+	printk(KERN_NOTICE "Get-Time: register_device() is called.");
+	register_num = register_chrdev(0, dev_name, &my_f);
+	if(register_num < 0)
+	{
+		printk(KERN_WARNING "Get-Time: Failed to register with error %i", register_num);
+	}
+	device_mj_num = register_num;
+	printk(KERN_NOTICE "Get-Time: registered device major number is %i", device_mj_num);
+	return SUCCESS;
+}
+
+/* Unregistering a device */
+void unregister_device(void)
+{
+	printk(KERN_NOTICE "Get-Time: unregister_device() is called");
+	if(device_mj_num != 0)
+	{
+		unregister_chrdev(device_mj_num, dev_name);
+	}
+}
+/* To unload a linux driver */
+static void my_exit(void)
+{
+	unregister_device();
+	return;
+}
+
+//module_init(my_init);
+//module_exit(my_exit);
---
+++ myOmerPgm/userMyTime.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/syscall.h>
+#include <sys/unistd.h>
+#include <fcntl.h>
+
+#define N 30
+#define MAX_LENGTH 400
+
+int main(int argc, char *argv[])
+{
+int i, fd, bytes_read;
+struct timeval gtodTimes[N];
+
+/* allocate memory for character buffers HERE before you use them */
+char *procClockTimes = malloc (sizeof (char) * N);
+
+fd = open("/dev/mytime", O_RDONLY);
+/* check for errors HERE */
+if(fd < 0)
+{
+	perror("Error opening file");
+}
+
+for(i=0; i<N; i++)
+{
+	gettimeofday(&gtodTimes[i], 0);
+	bytes_read = read(fd, procClockTimes[i], MAX_LENGTH);
+	/* check for errors HERE */
+	if(bytes_read < 0)
+	{
+		perror("Read is wrong");			
+	}		
+}
+
+close(fd);
+
+for(i=0; i<N; i++)
+{
+	printf("%ld %s", gtodTimes[i].tv_usec, procClockTimes[i]);
+	/* fix the output format appropriately in the above line */
+}
+
+}
